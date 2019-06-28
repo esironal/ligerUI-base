55bb4546dda383d3c7ed46121e031f54666507f4
esironal <lanorise333@gmail.com>
2019-06-28 17:55:42 -0400

    up

--- indexdata.js
+++ indexdata.js
@@ -9,6 +9,100 @@ var indexdata =
 	]
     }, 
     
+{ text: 'Notes', isexpand: false, children: [
+		{url:"https://glot.io/users/esironal/snippets", text: "Snippets"},
+		{url:"https://etherpad.wikimedia.org/p/esironal-notes",text:"esironal-notes"},
+		{url:"https://codeshare.io/5zqZJE",text:"Notes-codeshare"},
+		{url:"http://socrates.io/#lk4o9gz",text:"socrates-lk4o9gz"}
+	]
+    }, 
+    
+
+
+//  {url:"http://emscripten.org/",text:"Emscripten"},
+//  {url:"http://emscripten.org/",text:"Emscripten"},
+//  {url:"http://emscripten.org/",text:"Emscripten"},
+//  {url:"http://emscripten.org/",text:"Emscripten"},
+//  {url:"http://emscripten.org/",text:"Emscripten"},
+//  {url:"http://emscripten.org/",text:"Emscripten"},
+
+
+
+	{ text: 'Libraries', isexpand: false, children: [
+		
+		{url:"https://www.jsdelivr.com",text:"Jsdelivr"},
+		{url:"https://devdocs.io/",text:"devdocs.io"},
+		{url:"https://www.webcomponents.org/",text:"WebComponents"},
+		{url:"https://rsuitejs.com/en/guide/introduction", text: "Rsuite Js"},
+		{url:"https://nodejs.org/docs/latest-v8.x/api/index.html",text:"nodejs Api"},
+		{url:"http://emscripten.org/",text:"Emscripten"},
+		{url:"https://esironal.github.io/microjs",text:"Microjs"},
+		{url:"https://esironal.github.io/string.js",text:"string-js"},
+		{url:"http://ksylvest.github.io/jquery-gridly/",text:"Gridly"},
+		{url:"https://vuetifyjs.com/en/getting-started/quick-start",text:"vuetifyjs"},
+		{url:"http://esironal.github.com/git-guide",text:"Git Guide"}
+	]
+    }, 
+    //  {url:"http://emscripten.org/",text:"Emscripten"},
+//  {url:"http://emscripten.org/",text:"Emscripten"},
+//  {url:"http://emscripten.org/",text:"Emscripten"},
+//  {url:"http://emscripten.org/",text:"Emscripten"},
+//  {url:"http://emscripten.org/",text:"Emscripten"},
+//  {url:"http://emscripten.org/",text:"Emscripten"},
+
+
+
+
+
+
+
+
+
+	{ text: 'IDEs', isexpand: false, children: [
+		{url:"https://exlcode.com/ide",text:"Exlcode IDE"},
+		{url:"https://playcode.io/online-html-editor",text:"PlayCode"},
+		{url:"https://webpaw.net/",text:"Web Paw"},
+		{url:"http://emscripten.org/",text:"Emscripten"}
+	]
+    }, 
+
+
+	//  {url:"http://emscripten.org/",text:"Emscripten"},
+//  {url:"http://emscripten.org/",text:"Emscripten"},
+//  {url:"http://emscripten.org/",text:"Emscripten"},
+//  {url:"http://emscripten.org/",text:"Emscripten"},
+//  {url:"http://emscripten.org/",text:"Emscripten"},
+//  {url:"http://emscripten.org/",text:"Emscripten"},
+//  {url:"http://emscripten.org/",text:"Emscripten"},
+
+
+	{ text: 'Desks', isexpand: false, children: [
+		{url:"https://esironal.github.io/win10-ui/", text: "Win10 UI"},
+		{url:"https://esironal.github.io/ylui-desk/",text:"ylui-desk"},
+		{url:"https://esironal.github.io/china-desk/",text:"china-desk"}
+	]
+    }, 
+
+	{ text: 'Libs',isexpand:false, children: [ 
+		{url:"https://codetogo.io/all",text:"codetogo.io"},
+		{url:"https://free-for.dev/",text:"free-for.dev"},
+		{url:"http://kevinbalicot.github.io/megastores.js/",text:"Megastores"},
+		{url:"https://quasar-framework.org/guide/index.html",text:"Quasar"},
+		{url:"https://quasar-framework.org/quasar-play/android/index.html#/showcase",text:"Quasar-play"},
+		{url:"https://nodeschool.io/",text:"nodeschool.io"},
+		{url:"http://cyber.wizard.institute/",text:"Cyber-Institute"},
+		{url:"https://unpkg.com/domo@0.5.9/index.html",text:"domo"},
+		{url:"https://datproject.org/",text:" Dat Project"},
+		{url:"https://aurelia.io/docs/tutorials/creating-a-todo-app#rendering-the-app",text:"aurelia.io"},
+		{url:"https://preactjs.com/",text:"Preact js"},
+		{url:"https://hackr.io/tutorials/learn-vue-js?sort=upvotes&type_tags%5B%5D=1",text:"learn-vue-js"},
+		{url:"https://shiffman.net/a2z/",text:"Programming A-Z"},
+		{url:"https://markojs.com/try-online",text:"markojs"}
+	]
+    },
+    
+
+
     { text: 'Tools', isexpand: true, children: [
 		{url:"https://esironal.github.io/GitHub_Code_Editor/#!from=github&owner=esironal&repo=ligerUI-base",text:"GitHub_Code"},
 		{url:"https://www.sharedrop.io/",text:"sharedrop.io"},
@@ -43,41 +137,8 @@ var indexdata =
 		{url:"http://0.0.0.0:6460/index.php?explorer&type=iframe&path=/mnt/Lan-Dev/Gitload/ligerUI-base/",text:"Explorer"}
 	]
     }, 
-    { text: 'Notes', isexpand: false, children: [
-		{url:"https://glot.io/users/esironal/snippets", text: "Snippets"},
-		{url:"https://etherpad.wikimedia.org/p/esironal-notes",text:"esironal-notes"},
-		{url:"https://codeshare.io/5zqZJE",text:"Notes-codeshare"},
-		{url:"http://socrates.io/#lk4o9gz",text:"socrates-lk4o9gz"}
-	]
-    }, 
     
-    { text: 'Libs',isexpand:false, children: [ 
-		{url:"https://codetogo.io/all",text:"codetogo.io"},
-		{url:"https://free-for.dev/",text:"free-for.dev"},
-		{url:"https://devdocs.io/",text:"devdocs.io"},
-		{url:"https://www.webcomponents.org/",text:"WebComponents"},
-		{url:"https://www.jsdelivr.com",text:"Jsdelivr"},
-		{url:"http://ksylvest.github.io/jquery-gridly/",text:"Gridly"},
-		{url:"http://kevinbalicot.github.io/megastores.js/",text:"Megastores"},
-		{url:"https://quasar-framework.org/guide/index.html",text:"Quasar"},
-		{url:"https://quasar-framework.org/quasar-play/android/index.html#/showcase",text:"Quasar-play"},
-		{url:"https://nodeschool.io/",text:"nodeschool.io"},
-		{url:"http://cyber.wizard.institute/",text:"Cyber-Institute"},
-		{url:"https://esironal.github.io/microjs",text:"Microjs"},
-		{url:"https://nodejs.org/docs/latest-v8.x/api/index.html",text:"nodejs Api"},
-		{url:"https://esironal.github.io/string.js",text:"string-js"},
-		{url:"https://unpkg.com/domo@0.5.9/index.html",text:"domo"},
-		{url:"https://datproject.org/",text:" Dat Project"},
-		{url:"https://aurelia.io/docs/tutorials/creating-a-todo-app#rendering-the-app",text:"aurelia.io"},
-		{url:"https://preactjs.com/",text:"Preact js"},
-		{url:"https://hackr.io/tutorials/learn-vue-js?sort=upvotes&type_tags%5B%5D=1",text:"learn-vue-js"},
-		{url:"https://vuetifyjs.com/en/getting-started/quick-start",text:"vuetifyjs"},
-		{url:"https://shiffman.net/a2z/",text:"Programming A-Z"},
-		{url:"https://markojs.com/try-online",text:"markojs"}
-	]
-    },
     
-
     { text: 'Dev',isexpand:false, children: [ 
 		{url:"ckeditor-4-cdn.html",text:"Ckeditor"},
 		{url:"https://esironal.github.io/apps/code-sketch/",text:"Code Sketch"},
@@ -87,7 +148,6 @@ var indexdata =
 		{url:"https://esironal.github.io/js-code-to-svg-flowchart/docs/live-editor/index.html",text:"JS flowchart"},
 		{url:"https://esironal.github.io/apps/codemagic/",text:"CodeMagic"},
 		{url:"http://jsonviewer.stack.hu",text:"Json Viewer"},
-		{url:"https://playcode.io/online-html-editor",text:"PlayCode"},
 		{url:"https://webassembly.studio/",text:"Webassembly"},
 		{url:"https://multifiddle.ml",text:"Multi-Fiddle"},
 		{url:"https://codepen.io/esironal/pens/public/",text:"Codepen"},
@@ -97,7 +157,6 @@ var indexdata =
 		{url:"https://kangax.github.io/html-minifier/",text:"html-minifier"},
 		{url:"http://resizeimage.net/",text:"resizeimage"},
 		{url:"https://hashbase.io/",text:"hashbase.io"},
-		{url:"https://exlcode.com/ide",text:"Exlcode IDE"},
 		{url:"https://esironal.github.io/Editor-3",text:"Editor 3"},
 		{url:"https://esironal.github.io/angular fiddle",text:"angular fiddle"},
 		{url:"https://esironal.github.io/JSONedit",text:"JSONedit"},
